50cee68b211268fb7c6bf0b97ccc685df91924a4
mgruben <GrubenM@gmail.com>
2016-12-12 00:39:45 -0600

    Begin each-run looping outer logic

--- process.py
+++ process.py
@@ -4,3 +4,15 @@ import pickle
 # For feeding the data to the Keras model
 import numpy as np
 
+# For reading captured .jpg files into numpy arrays
+from scipy import misc
+
+# For checking files and paths
+import os
+
+im_array = []
+
+for track in ['T1', 'T2']:
+    for run in ['R01', 'R02', 'R03', 'R04', 'R05', 'R06', 'R07', 
+                'R08', 'R09', 'R10', 'R11', 'R12', 'R13', 'R014']:
+    
