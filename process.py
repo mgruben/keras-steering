e5456454574d07e7f8216898172ec463d4f8e29b
mgruben <GrubenM@gmail.com>
2016-12-12 01:40:25 -0600

    Replace pickling with dataframe load

--- process.py
+++ process.py
@@ -13,34 +13,28 @@ import os
 # For reading csv output
 import pandas as pd
 
-def makeInputPickle():
+def logsToDataframe():
     '''
     Depending on the size of your input, this could exhaust memory :(
     '''
     
-    im_array = []
-
+    merged_df = pd.DataFrame()
+    
     for track in ['T1', 'T2']:
         for run in ['R01', 'R02', 'R03', 'R04', 'R05', 'R06', 'R07', 
                     'R08', 'R09', 'R10', 'R11', 'R12', 'R13', 'R14']:
             if not os.path.isdir(track + '/' + run):
                 continue
             else:
-                fp = track + '/' + run + '/' + 'IMG'
-                imgs = os.listdir(fp)
-                for f in imgs:
-                    if 'center' in f:
-                        im_array.append(misc.imread(fp + '/' + f))
-    im_array = np.array(im_array)
+                fp = track + '/' + run + '/' + 'driving_log.csv'
+                df = pd.read_csv(fp, usecols=[0,3], names=['X_file','y_angle'])
+                merged_df = merged_df.append(df, ignore_index=True)
     
-    pf = 'pickles/X.p'
-    try:
-        pd = open(pf, 'wb')
-        pickle.dump(np.asarray(im_array), pd, pickle.HIGHEST_PROTOCOL)
-        pd.close()
-    except Exception as e:
-        print('Unable to save data to', pf, ':', e)
-        raise
+    return merged_df
+    
+def makePickle(filename, dataframe, column)    
+    pf = 'pickles/' + filename
+    dataframe.to_pickle(pf)
 
 def makeLabelsPickle():
     '''
