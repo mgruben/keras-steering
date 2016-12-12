ff1bda031a39d8165d3cc18b6db49c3e07856697
mgruben <GrubenM@gmail.com>
2016-12-12 01:19:50 -0600

    Define pickling helper function

--- process.py
+++ process.py
@@ -10,9 +10,40 @@ from scipy import misc
 # For checking files and paths
 import os
 
-im_array = []
+# For reading csv output
+import pandas as pd
 
-for track in ['T1', 'T2']:
-    for run in ['R01', 'R02', 'R03', 'R04', 'R05', 'R06', 'R07', 
-                'R08', 'R09', 'R10', 'R11', 'R12', 'R13', 'R014']:
+def makeInputPickle():
+    '''
+    Depending on the size of your input, this could exhaust memory :(
+    '''
+    
+    im_array = []
+
+    for track in ['T1', 'T2']:
+        for run in ['R01', 'R02', 'R03', 'R04', 'R05', 'R06', 'R07', 
+                    'R08', 'R09', 'R10', 'R11', 'R12', 'R13', 'R14']:
+            if not os.path.isdir(track + '/' + run):
+                continue
+            else:
+                fp = track + '/' + run + '/' + 'IMG'
+                imgs = os.listdir(fp)
+                for f in imgs:
+                    if 'center' in f:
+                        im_array.append(misc.imread(fp + '/' + f))
+    im_array = np.array(im_array)
+    
+    pf = 'pickles/X.p'
+    try:
+        pd = open(pf, 'wb')
+        pickle.dump(np.asarray(im_array), pd, pickle.HIGHEST_PROTOCOL)
+        pd.close()
+    except Exception as e:
+        print('Unable to save data to', pf, ':', e)
+        raise
+
+def makeLabelsPickle():
+    '''
+    '''
+    
     
